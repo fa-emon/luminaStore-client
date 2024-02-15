3a8e9b62e583300ca5075d086fcbc997c9ed82fd
Farhan933 <farhanfarabiemon@gmail.com>
2024-02-15 20:23:42 +0600

    adding graph chart

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -5,7 +5,11 @@ import { IoWallet } from "react-icons/io5";
 import { GiClothes } from "react-icons/gi";
 import { FaUsers } from "react-icons/fa";
 import { MdOutlineDeliveryDining } from "react-icons/md";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, PieChart, Pie, Legend } from 'recharts';
 
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const AdminHome = () => {
     const { user } = useAuth();
@@ -14,9 +18,49 @@ const AdminHome = () => {
     const { data: statistics = {} } = useQuery({
         queryKey: ['admin-statistics'],
         queryFn: async () => {
-            const res = await axiosSecure('/admin-statistics');
+            const res = await axiosSecure.get('/admin-statistics');
             return res.data;
         }
+    })
+
+    const { data: chartData = [] } = useQuery({
+        queryKey: ['order-statistics'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/order-statistics');
+            return res.data;
+        }
+    })
+
+    //custom shape for the bar chart.
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+        Z`;
+    };
+
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
+
+    //custom shape for the pie chart
+    const RADIAN = Math.PI / 180;
+    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN);
+        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+        return (
+            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+                {`${(percent * 100).toFixed(0)}%`}
+            </text>
+        );
+    };
+
+    const pieChartData = chartData.map(data => {
+        return {name: data.category, value: data.revenue}
     })
 
     return (
@@ -65,13 +109,57 @@ const AdminHome = () => {
 
                     <div className="bg-[#CBE8EE] rounded py-10 text-center hover:bg-[#0e0e0e] hover:text-white">
                         <div className="flex items-center justify-center">
-                            <MdOutlineDeliveryDining  className="text-6xl me-2 text-[#f1f1f1]"></MdOutlineDeliveryDining>
+                            <MdOutlineDeliveryDining className="text-6xl me-2 text-[#f1f1f1]"></MdOutlineDeliveryDining>
                             <div>
                                 <p className="text-3xl heading-font">{statistics.orders}</p>
                                 <p className="text-2xl all-font">ORDERS</p>
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+
+            <div className="w-full flex mt-10">
+                <div className="w-1/2">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {chartData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+                </div>
+                <div className="w-1/2">
+                    <PieChart width={400} height={300}>
+                        <Pie
+                            data={pieChartData}
+                            cx="50%"
+                            cy="50%"
+                            labelLine={false}
+                            label={renderCustomizedLabel}
+                            outerRadius={80}
+                            fill="#8884d8"
+                            dataKey="value"
+                        >
+                            {pieChartData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                        <Legend></Legend>
+                    </PieChart>
                 </div>
             </div>
         </>
